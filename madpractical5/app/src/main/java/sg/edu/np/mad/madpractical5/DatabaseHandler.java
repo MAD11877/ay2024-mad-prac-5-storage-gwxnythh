dbbdac2ae4d4dc63bcf47268431442d1f3fe1f6a
gwxnythh <S10262576@connect.np.edu.sg>
2024-05-26 15:50:35 +0800

    Initial Project Setup

---
+++ madpractical5/app/src/main/java/sg/edu/np/mad/madpractical5/DatabaseHandler.java
@@ -0,0 +1,12 @@
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+public class MyDBHandler extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+    }
+}
